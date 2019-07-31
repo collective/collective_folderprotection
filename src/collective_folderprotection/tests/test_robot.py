4c5270492ae33d4cc79986b0b9b90247ba76bbdf
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2019-07-31 15:13:44 -0500

    Change extension for robot test files to robot

--- src/collective_folderprotection/tests/test_robot.py
+++ src/collective_folderprotection/tests/test_robot.py
@@ -8,15 +8,15 @@ import unittest
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
-        layered(robotsuite.RobotTestSuite("manager_functional.txt"),
+        layered(robotsuite.RobotTestSuite("manager_functional.robot"),
                 layer=COLLECTIVE_FOLDERPROTECTION_FUNCTIONAL_TESTING)
     ])
     suite.addTests([
-        layered(robotsuite.RobotTestSuite("owner_functional.txt"),
+        layered(robotsuite.RobotTestSuite("owner_functional.robot"),
                 layer=COLLECTIVE_FOLDERPROTECTION_FUNCTIONAL_TESTING)
     ])
     suite.addTests([
-        layered(robotsuite.RobotTestSuite("member_functional.txt"),
+        layered(robotsuite.RobotTestSuite("member_functional.robot"),
                 layer=COLLECTIVE_FOLDERPROTECTION_FUNCTIONAL_TESTING)
     ])
     return suite
